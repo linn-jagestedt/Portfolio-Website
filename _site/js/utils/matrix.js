eea60b0f30c36c5bbd74d25c52a702ea583c5492
Linn <32527868+linn-jagestedt@users.noreply.github.com>
2024-02-17 18:36:48 +0100

    Delete _site directory

--- _site/js/utils/matrix.js
+++
@@ -1,9 +0,0 @@
-export default function createMaterix(scaleX, scaleY, scaleZ, positionX, positionY, positionZ) 
-{
-	return [
-        scaleX, 0, 0, positionX,
-        0, scaleY, 0, positionY,
-        0, 0, scaleZ, positionZ,
-        0, 0, 0, 1
-    ]
-}
